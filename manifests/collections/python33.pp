b4ca69e4fb9a2b6091bcacbbe9555ff9f3e8965f
Geoff Garbers <geoff@totalsend.com>
2015-09-09 10:35:52 +0200

    Added configurable packages for python33 collection.

--- manifests/collections/python33.pp
+++ manifests/collections/python33.pp
@@ -1,9 +1,13 @@
 class scl::collections::python33
+(
+	$packages = "python33"
+)
+
 {
 	::scl::collection { "scl-python33":
 		repo_descr => "SCL - Python 3.3",
 		repo_baseurl => 'https://www.softwarecollections.org/repos/rhscl/python33/epel-$releasever-x86_64',
 		os_versions => [ 6, 7 ],
-		package_install => "python33",
+		package_install => $packages,
 	}
 }
