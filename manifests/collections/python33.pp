a426f7c3fc6969a3640fd7889a5ece3d158a33d4
Geoff Garbers <geoff@totalsend.com>
2015-09-10 12:16:30 +0200

    Updated Python SCL description. Updated metadata.

--- manifests/collections/python33.pp
+++ manifests/collections/python33.pp
@@ -5,7 +5,7 @@ class scl::collections::python33
 
 {
 	::scl::collection { "scl-python33":
-		repo_descr => "SCL - Python 3.3",
+		repo_descr => "SCL - Python 3.3.2",
 		repo_baseurl => 'https://www.softwarecollections.org/repos/rhscl/python33/epel-$releasever-x86_64',
 		os_versions => [ 6, 7 ],
 		package_install => $packages,
